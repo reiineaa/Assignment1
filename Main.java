3c265548e48f497a3fede1a7b66c6011c3a2d474
reiineaa <80281865+reiineaa@users.noreply.github.com>
2021-08-16 23:44:53 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         List<Point>points=new ArrayList<>();
         try{
-            BufferedReader br=new BufferedReader(new FileReader("C:\\Users\\Lenovo\\Desktop\\file1.txt"));
             String fileRead=br.readLine();
             while(fileRead!=null){
                 String[]line=fileRead.split(",");
